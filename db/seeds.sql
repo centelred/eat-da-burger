f9a0e92f76d01e55d43f2ecd96950fe0655811de
Curtis Coddington <hccoddington@gmail.com>
2017-05-03 11:09:18 -0400

    Add files via upload

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO burgers(burger_name) VALUES ('Single Cheeseburger');
+INSERT INTO burgers(burger_name) VALUES ('Duburger');
+INSERT INTO burgers(burger_name) VALUES ('Triple Cheeseburger');
+INSERT INTO burgers(burger_name) VALUES ('Quadburger');
